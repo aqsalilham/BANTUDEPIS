eb2992e550ce54a322b75fd76409fcc2fa55b667
aqsalilham <58905154+aqsalilham@users.noreply.github.com>
2019-12-17 06:06:43 +0700

    Update bantudepis.sh

--- bantudepis.sh
+++ bantudepis.sh
@@ -1,9 +1,164 @@
 #!/bin/bash
-#version 1.0
-
-clear
+#screetsec@dracos-linux.org
 
 
+VERSION='1.1.1'
+CODENAME='Pretty'
+dir='pwd'
+
+
+#This colour
+cyan='\e[0;36m'
+green='\e[1;32m'
+okegreen='\033[92m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+BlueF='\e[1;34m'
+
+#Accurate method to detect user Distro
+lnx="Linux"
+lsb_release -i > temp/distro.tmp
+lsb_release -c > temp/codename.tmp
+rlname=`awk '{print $2}' temp/codename.tmp`
+dist=`awk '{print $3}' temp/distro.tmp`
+dist1=`awk '{print $4}' temp/distro.tmp`
+rm -f temp/codename.tmp >/dev/null
+rm -f temp/distro.tmp >/dev/null
+if [ "$dist" == "$lnx" ];
+then
+dist0=$dist1
+else
+dist0=$dist
+fi
+OS=`uname` #
+distro=$dist0
+disrov=$rlname
+
+
+cont () {
+# check medusa
+      which /usr/bin/medusa > /dev/null 2>&1
+      if [ "$?" -eq "0" ]; then
+      echo -e $okegreen [✔]::[Medusa: Installation found!;
+else
+   echo -e $red [x]::[warning]:this script require medusa installed to work ;
+   echo ""
+   echo -e $red [!]::Please Install medusa to work ;
+   sleep 0.5
+   exit 1
+ fi
+sleep 0.5
+
+
+# check crunch
+      which tools/crunch-3.6/crunch > /dev/null 2>&1
+      if [ "$?" -eq "0" ]; then
+      echo -e $okegreen [✔]::[Crunch]: Installation found!;
+
+else
+
+   echo -e $red [x]::[warning]:this script require crunch installed to work ;
+   echo ""
+   echo -e $red [!]::check the folder tools crunch ;
+   echo ""
+   sleep 0.5
+   exit 1
+fi
+sleep 0.5
+
+# check cewl
+      which /usr/bin/cewl > /dev/null 2>&1
+      if [ "$?" -eq "0" ]; then
+      echo -e $okegreen [✔]::[Cewl]: Installation found!;
+
+else
+
+   echo -e $red [x]::[warning]:this script require cewl installed to work ;
+   echo ""
+   echo -e $red [!]::Please install cewl to work ;
+   echo ""
+   sleep 0.5
+   exit 1
+fi
+sleep 0.5
+
+# check fcrackzip
+      which /usr/bin/fcrackzip > /dev/null 2>&1
+      if [ "$?" -eq "0" ]; then
+      echo -e $okegreen [✔]::[fcrackzip]: Installation found!;
+else
+
+   echo -e $red [x]::[warning]:this script require fcrackzip installed to work ;
+   echo ""
+   echo -e $red [!]::Please install fcrackzip to work properly ;
+   echo ""
+   sleep 0.5
+   exit 1
+fi
+sleep 0.5
+
+# check dirsearch
+      which tools/dirsearch/dirsearch.py > /dev/null 2>&1
+      if [ "$?" -eq "0" ]; then
+      echo -e $okegreen [✔]::[dirsearch]: Installation found!;
+else
+
+   echo -e $red [x]::[warning]:this script require dirsearch installed to work ;
+   echo ""
+   echo -e $red [!]::Please install dirsearch to work properly ;
+   echo ""
+   sleep 0.5
+   exit 1
+fi
+sleep 0.5
+
+}
+
+
+function chknet() {
+echo -e $red "[X] Your Internet is not working correctly!"
+sleep 0.5
+echo -e $okegreen "[*] Checking ...."
+ping -c 1 8.8.4.4 > /dev/null 2>&1
+png="$?"
+ if [ $png == "0" ]
+then
+    echo -e $red "[X] Your linux OS is not able to resolve"
+    echo -e $red "hostnames over terminal using ping !!"
+    echo ""
+    echo -e $yellow "Search on the web : (unable to resolve hostnames ping) to find a solution"
+echo ""
+echo -e $okegreen "BrutSploit will continue , but some of its functions may not work properly."
+echo ""
+echo -e $okegreen "Press [ENTER] key to continue"
+read continue
+cont
+    sleep 0.5
+elif [ $png == "1" ]
+then
+    echo -e $yellow "You are connected to your local network but not to the web ."
+    echo -e $yellow "Check if your router/modem gateway is connected to the web ."
+echo ""
+echo -e $okegreen "BrutSploit will continue , but some of its functions may not work properly."
+echo ""
+echo -e $okegreen "Press [ENTER] key to continue"
+read continue
+cont
+sleep 0.5
+elif [ $png == "2" ]
+then
+echo -e $red "You are not connected to any network ."
+echo ""
+echo -e $okegreen "BrutSploit will continue , but many of its functions will not work properly."
+echo ""
+echo -e $okegreen "Press [ENTER] key to continue"
+read continue
+cont
+    sleep 0.5
+fi
+}
 
 #########################
 #CHECK DEPENDICIES
@@ -14,13 +169,23 @@ echo "  | __  |___ _ _| |_ ___|   __|___| |___|_| |_ "
 echo "  | __ -|  _| | |  _| -_|__   | - | | - | |  _| "
 echo "  |_____|_| |___|_| |___|_____|  _|_|___|_|_| "
 echo "                              |_|             "
-echo -e $lightgreen'-- -- +=[(c) cuma depis | LOVE YOU DEPIS | JANCOK MUMET | ASUU PINGIN NANGIS'
-echo -e $cyan'-- -- +=[ Author: HAMBA ALLAH < YANG PALING SEKSIH >  ]=+ -- -- '
+echo -e $lightgreen'-- -- +=[(c) cuma depis | i love you depis | Pusing | Mumet cok '
+echo -e $cyan'-- -- +=[ Author: hamba allah < paling seksih >  ]=+ -- -- '
 echo -e " "
 
+if [ $(id -u) != "0" ]; then
 
+      echo -e $red [!]::[Check Dependencies] ;
+      sleep 0.5
+      echo -e $red [✔]::[Check User]: $USER ;
+      echo -e $red [✔]::[Distro]: $distro ;
+      echo -e $red [✔]::[Release]: $rlname ;
+      sleep 0.5
+      echo -e $red [x]::[not root]: you need to be [root] to run this script.;
+      echo ""
+   	  sleep 1
 	  exit
-sleep 1
+
 
 ###################################################
 # CTRL + C
